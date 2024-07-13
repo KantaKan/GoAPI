0f94ac8d69802a6c3de34a720b9f472533dedd2d
fiadaze <fiakantapon@gmail.com>
2024-07-14 03:01:07 +0700

    hello todo go

--- main.go
+++ main.go
@@ -7,9 +7,38 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct{
+	ID int `json:"id"`
+	Completed bool `json:"completed"`
+	Bpdy string `json:"body"`
+}
+
 func main() {
 	fmt.Println("hello world")
 	app := fiber.New()
+	
+	todos := []Todo{}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := Todo{}
+
+		if err := c.BodyParser(todo){
+			return err
+		}
+
+		if todo.body == "" {
+			return c.Status(400).JSON(fiber.Map{"error":"body is required"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos,*todo)
+
+		return c.Status(201).JSON(todo) //created todo
+	})
 
 	log.Fatal(app.Listen(":4000"))
 }
